fcb0c42831675e77559584d5d35f3027f8e56736
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-13 19:26:54 +0700

    Create cart page

---
+++ app/(front)/cart/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CartDetails from './CartDetails'
+
+export const metadata = {
+  title: 'Shopping Cart',
+}
+
+export default function Cartpage() {
+  return <CartDetails />
+}
