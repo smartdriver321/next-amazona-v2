7008e0dc611a0cad2fcf029724cd8fa3ddb583b9
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-01-15 11:35:29 +0700

    Load products from database

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,14 +1,30 @@
 import AddToCart from '@/components/products/AddToCart'
-import data from '@/lib/data'
+import { convertDocToObj } from '@/lib/utils'
+import productService from '@/lib/services/productService'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function ProducrDetails({
+export async function generateMetadata({
   params,
 }: {
   params: { slug: string }
 }) {
-  const product = data.products.find((x) => x.slug === params.slug)
+  const product = await productService.getBySlug(params.slug)
+  if (!product) {
+    return { title: 'Product not found' }
+  }
+  return {
+    title: product.name,
+    description: product.description,
+  }
+}
+
+export default async function ProductDetails({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await productService.getBySlug(params.slug)
 
   if (!product) {
     return <div>Product not found!</div>
@@ -66,7 +82,12 @@ export default function ProducrDetails({
               {product.countInStock !== 0 && (
                 <div className='card-actions justify-center'>
                   <AddToCart
-                    item={{ ...product, qty: 0, color: '', size: '' }}
+                    item={{
+                      ...convertDocToObj(product),
+                      qty: 0,
+                      color: '',
+                      size: '',
+                    }}
                   />
                 </div>
               )}
